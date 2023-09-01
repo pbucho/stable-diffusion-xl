f9822c9f41a7819de0898d71ef8c0014e57221f4
Furkan Gözükara <furkangozukara@gmail.com>
2023-09-02 02:00:21 +0300

    Update app7.py

--- app7.py
+++ app7.py
@@ -18,11 +18,11 @@ access_token = os.getenv("ACCESS_TOKEN")
 
 if model_dir:
     # Use local model
-    model_key_base = os.path.join(model_dir, "stable-diffusion-xl-base-0.9")
-    model_key_refiner = os.path.join(model_dir, "stable-diffusion-xl-refiner-0.9")
+    model_key_base = os.path.join(model_dir, "stabilityai/stable-diffusion-xl-base-1.0")
+    model_key_refiner = os.path.join(model_dir, "stabilityai/stable-diffusion-xl-refiner-1.0")
 else:
-    model_key_base = "stabilityai/stable-diffusion-xl-base-0.9"
-    model_key_refiner = "stabilityai/stable-diffusion-xl-refiner-0.9"
+    model_key_base = "stabilityai/stable-diffusion-xl-base-1.0"
+    model_key_refiner = "stabilityai/stable-diffusion-xl-refiner-1.0"
 
 # Use refiner (enabled by default)
 enable_refiner = os.getenv("ENABLE_REFINER", "true").lower() == "true"
