1d3e0a15cab71ee4dcf2ef10e5ed942bebf73bba
Furkan Gözükara <furkangozukara@gmail.com>
2023-09-02 04:14:49 +0300

    Update app7.py

--- app7.py
+++ app7.py
@@ -13,16 +13,11 @@ from share_btn import community_icon_html, loading_icon_html, share_js
 
 # SDXL code: https://github.com/huggingface/diffusers/pull/3859
 
-model_dir = '/workspace'
+model_dir = "/workspace/"
 access_token = os.getenv("ACCESS_TOKEN")
 
-if model_dir:
-    # Use local model
-    model_key_base = os.path.join(model_dir, "stable-diffusion-xl-base-1.0")
-    model_key_refiner = os.path.join(model_dir, "stable-diffusion-xl-refiner-1.0")
-else:
-    model_key_base = "stabilityai/stable-diffusion-xl-base-1.0"
-    model_key_refiner = "stabilityai/stable-diffusion-xl-refiner-1.0"
+model_key_base = "stabilityai/stable-diffusion-xl-base-1.0"
+model_key_refiner = "stabilityai/stable-diffusion-xl-refiner-1.0"
 
 # Use refiner (enabled by default)
 enable_refiner = os.getenv("ENABLE_REFINER", "true").lower() == "true"
@@ -308,7 +303,7 @@ with block:
                   <rect x="23" y="69" width="23" height="23" fill="black"></rect>
                 </svg>
                 <h1 style="font-weight: 900; margin-bottom: 7px;margin-top:5px">
-                  Stable Diffusion XL 0.9 Demo
+                  Stable Diffusion XL 1.0
                 </h1>
               </div>
               <p style="margin-bottom: 10px; font-size: 94%; line-height: 23px;">
@@ -318,7 +313,7 @@ with block:
 				<br/>
 				Patreon exclusive posts index : <a style="text-decoration: underline;" href="https://github.com/FurkanGozukara/Stable-Diffusion/blob/main/Patreon-Posts-Index.md">https://github.com/FurkanGozukara/Stable-Diffusion/blob/main/Patreon-Posts-Index.md</a>
 				<br/>
-                Stable Diffusion XL 0.9 is the latest text-to-image model from StabilityAI. 
+                Stable Diffusion XL 1.0 is the latest text-to-image model from StabilityAI. 
                 <br/>
                 For faster generation and API access you can try
                 <a
